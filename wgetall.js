252a1fec36f204deb0eca7078bdee512f9662475
MaWo2 <30822852+MaWo2@users.noreply.github.com>
2022-07-22 15:29:19 +0200

    Add files via upload

--- wgetall.js
+++ wgetall.js
@@ -10,6 +10,21 @@ export async function main(ns) {
 	//will be pulled into the same folder structure
 	filesToPull = [
 		["", "monitor.js"],
+		["", "purchaseServer8GB.js"],
+		["simplehack/", "simplehack-self.js"],
+		["simplehack/", "simplehack-template.js"],
+		["loophack/", "grow_loop.js"],
+		["loophack/", "grow_once.js"],
+		["loophack/", "hack_loop.js"],
+		["loophack/", "hack_once.js"],
+		["loophack/", "weaken_loop.js"],
+		["loophack/", "weaken_once.js"],
+		["loophack/", "loop-queen-pserv.js"],
+		["loophack/", "loop-queen-outside.js"],
+		["batchhack", "hwgw-queen.js"],
+		["batchhack", "b-grow.js"],
+		["batchhack", "b-hack.js"],
+		["batchhack", "b-weaken.js"],
 		["data-mining/", "world-profiler.js"],
 		["data-mining/", "func_world-scanner.js"]
 	]
