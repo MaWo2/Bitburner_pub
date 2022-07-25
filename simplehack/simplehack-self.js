642208cae6eeb822512ea6b13244074468a1aa38
MaWo2 <30822852+MaWo2@users.noreply.github.com>
2022-07-25 11:19:51 +0200

    Update simplehack-self.js

--- simplehack/simplehack-self.js
+++ simplehack/simplehack-self.js
@@ -5,14 +5,22 @@ export async function main(ns) {
 	var target = ns.getHostname();
 	var securityThreshold = 1; //e.g. 1
 	var moneyThreshold = 0.9; //e.g. 0.9
+	var isFaction = false;
 	//list of faction servers, because those cannot be hacked (money = 0)
 	//if server is faction server, switch target to harakiri-sushi
 	var factionServers = ["CSEC", "avmnite-02h", "I.I.I.I", "run4theh111z", "." ,"The-Cave", "w0r1d_d43m0n"];
 	if (factionServers.includes(target)) {
 		target = "harakiri-sushi";
+		isFaction = true;
 	}
 	
 	//assume, target is already nuked
+	
+	//initial loop to generate some early-game income
+	//hack server to 1% money
+	while ((isFaction == false) && (ns.getServerMoneyAvailable(target) > ns.getServerMaxMoney(target) * 0.01)) {
+		await ns.hack(target);
+	}
 
 	while (true) {
 		if (ns.getServerSecurityLevel(target) > ns.getServerMinSecurityLevel(target) + securityThreshold) {
