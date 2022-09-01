69b9dafa68a936f0ee05f0cfefbeedfe9813b411
MaWo2 <30822852+MaWo2@users.noreply.github.com>
2022-09-01 08:17:49 +0200

    Update wgetall.js

--- wgetall.js
+++ wgetall.js
@@ -19,6 +19,7 @@ export async function main(ns) {
 		["", "lazystarter.js"],
 		["", "folder-scp.js"],
 		["", "connect-to.js"],
+		["", "stopworld.js"],
 		["simplehack/", "simplehack-self.js"],
 		["simplehack/", "simplehack-template.js"],
 		["loophack/", "grow_loop.js"],
