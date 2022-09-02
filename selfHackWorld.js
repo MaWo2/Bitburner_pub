0c5a8975727592f40098abf2785b37e34238cabd
MaWo2 <30822852+MaWo2@users.noreply.github.com>
2022-09-02 15:38:14 +0200

    Update selfHackWorld.js

--- selfHackWorld.js
+++ selfHackWorld.js
@@ -8,12 +8,21 @@ export async function main(ns) {
 	var target = ns.read("ServersOfTheWorld.txt").split(",");
 	var playerHackingLevel = ns.getHackingLevel();
 	var attackScript = "/simplehack/simplehack-self.js"
+	var attackScript2 = "/simplehack/simplehack-template.js"
 	var scriptRAM = ns.getScriptRam(attackScript);
+	var scriptRAM2 = ns.getScriptRam(attackScript2);
 	for (let i = 0; i < target.length; i++) {
 		if (ns.getServerRequiredHackingLevel(target[i]) <= playerHackingLevel) {
-			var maxThreads = Math.floor((ns.getServerMaxRam(target[i]) - ns.getServerUsedRam(target[i]))/scriptRAM);
+			var maxThreads = Math.floor((ns.getServerMaxRam(target[i]) - ns.getServerUsedRam(target[i])) / scriptRAM);
 			if (maxThreads > 0) {
 				ns.exec(attackScript, target[i], maxThreads);
+			}
+		} else {
+			if (ns.hasRootAccess(target[i])) {
+				var maxThreads = Math.floor((ns.getServerMaxRam(target[i]) - ns.getServerUsedRam(target[i])) / scriptRAM2);
+				if (maxThreads > 0) {
+					ns.exec(attackScript2, target[i], maxThreads, "joesguns");
+				}
 			}
 		}
 	}
