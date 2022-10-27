2f93755ffc472aabe562e78fa1ba2218d1b6530b
MaWo2 <30822852+MaWo2@users.noreply.github.com>
2022-10-27 13:38:09 +0200

    Update world-profiler.js
    Adapted to changed write command in v2.1

--- data-mining/world-profiler.js
+++ data-mining/world-profiler.js
@@ -9,14 +9,27 @@
 import * as worldScan from 'data-mining/func_world-scanner.js'
 /** @param {NS} ns */
 export async function main(ns) {
-	var list = [];
+	var list;
 	var serverListFile = "/ServersOfTheWorld.txt";
 	var serverProfileFile = "/ServerProfiles.txt";
 	list = worldScan.scanner(ns, "home"); // the ns-package has to be passed to the scanner function
 	//ns.tprint(list);
 	
 	//write list to file
-	await ns.write(serverListFile, list, "w");
+	//since BBv2 this does not work any longer, because the command expects a string, but receives an object
+	//generate string first, then write to file
+	//await ns.write(serverListFile, list, "w");
+	// create empty file or overwrite existing one
+	await ns.write(serverListFile, "", "w");
+	var serverNames = "";
+	for (let server in list) {
+		serverNames += list[server] + ",";
+	}
+	//remove "," behind last name from string
+	var outString = serverNames.substring(0, serverNames.length - 1);
+	await ns.write(serverListFile, outString, "a" );
+
+
 	//create empty profile file or overwrite existing file with ""
 	await ns.write(serverProfileFile, "", "w");
 	var serverProfileInfo;
