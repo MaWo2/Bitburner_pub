36c8ecfc1f72f69c82b9a68622c5a84b2e5c4e83
MaWo2 <30822852+MaWo2@users.noreply.github.com>
2022-12-01 16:16:21 +0100

    Create 32GB_autostart.js

---
+++ 32GB_autostart.js
@@ -0,0 +1,77 @@
+/**
+ * Intention:
+ * - start things after an augmentation reset
+ * - use only 32 GB --> spawn other scripts
+ * 
+ * Basic plan:
+ * 1) set sleeves to study or work for faction
+ * 2) set player to study (or faction work)
+ * 3) buy hacknet server
+ * 4) buy TOR-router (if possible)
+ * 5) wait for hacking level 40
+ * 6) run lazystarter
+ * 
+ * Prerequisites:
+ * - SF4 (Singularity)
+ */
+/** @param {NS} ns */
+export async function main(ns) {
+	//delay, to make sure the game loaded properly
+	await ns.sleep(2000);
+	//check for faction invites
+	var factionsAvailable = ns.singularity.checkFactionInvitations();
+	//accept faction invitations, if there were any
+	if (factionsAvailable.length > 0) {
+		ns.toast("Accepting faction invitations.");
+		for (let faction in factionsAvailable) {
+			ns.singularity.joinFaction(factionsAvailable[faction]);
+		}
+	}
+
+	//set player to study at University
+	//LATER: Check, if faction is available, that we can work for
+	if (factionsAvailable.length > 0) {
+		//needs adaption for factions wo hacking...
+		ns.singularity.workForFaction(factionsAvailable[0], "Hacking Contracts", true);
+		ns.toast("Hacking for Faction.");
+	} else {
+		ns.toast("Studying...");
+		ns.singularity.universityCourse("Rothman University", "Study Computer Science", true);
+	}
+
+	//set sleeves to work
+	await ns.sleep(1000); //wait for factions to be properly available for sleeves?
+	ns.toast("Setting sleeves to work.");
+	ns.spawn("/sleeves/sleeve-starter.js", 1, "crime", true);
+
+	/**
+	//set player to study at University
+	//LATER: Check, if faction is available, that we can work for
+	if (factionsAvailable.length > 0) {
+		//needs adaption for factions wo hacking...
+		ns.singularity.workForFaction(factionsAvailable[0], "Hacking Contracts", true);
+		ns.toast("Hacking for Faction.");
+	} else {
+		ns.toast("Studying...");
+		ns.singularity.universityCourse("Rothman University", "Study Computer Science", true);
+	}
+	//buy hacknet server
+	//not available???
+	//buy TOR browser
+	//how to check for TOR price???
+	if (ns.getServerMoneyAvailable("home") >= 200000) {
+		ns.toast("Purchasing TOR browser.");
+		ns.singularity.purchaseTor();
+	}
+	//LATER: Check if we have at least one programme (e.g. BruteSSH.exe)
+	//if not, build it
+	//wait until player hacking level reached 40
+	ns.toast("Waiting for hacking level of 40.");
+	while (ns.getHackingLevel() < 40) {
+		await ns.sleep(1000);
+	}
+	//run lazystarter
+	ns.toast("Done. Handing over to lazystarter.");
+	ns.exec("lazystarter.js", "home", 1);
+	*/
+}
